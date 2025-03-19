d59b22367958aced083e34da1fedc6d3cb238be4
Vishal Gawde <vrgawde1@gmail.com>
2025-03-19 12:56:05 +0530

    Hilt initial Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    // Kotlin kapt
+    id("kotlin-kapt")
+    //Hilt
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -56,4 +60,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    // Kotlin kapt
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/example/grocery/GroceryApp.kt
@@ -0,0 +1,8 @@
+package com.example.grocery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GroceryApp: Application() {
+}
